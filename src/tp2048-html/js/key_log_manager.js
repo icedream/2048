5b37034a411b28f4cdbd54426da38ed13e808de8
icedream <icedream2k9@die-optimisten.net>
2014-04-03 16:43:49 +0200

    WebSocket stuff moved into separate js file.

--- src/tp2048-html/js/key_log_manager.js
+++ src/tp2048-html/js/key_log_manager.js
@@ -28,14 +28,3 @@ function log_hide_last() {
 }
 
 $(log_hide_last);
-
-$(function() {
-	var sock = new WebSocket("ws://localhost:19661/");
-	sock.onmessage = function (evt) 
-	{ 
-        var msg = $.parseJSON(evt.data);
-		if (game != null)
-			game.moveRemote(msg.issuer, msg.direction);
-	};
-	setInterval(function(){sock.send("{}")}, 5000);
-});
