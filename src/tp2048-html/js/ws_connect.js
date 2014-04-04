3cbd0b1a63591bcf69e9240789c7013da0f6397d
icedream <icedream2k9@die-optimisten.net>
2014-04-04 12:58:18 +0200

    Maintenance + chat connection disruption messages.

--- src/tp2048-html/js/ws_connect.js
+++ src/tp2048-html/js/ws_connect.js
@@ -1,10 +1,47 @@
-$(function() {
+$(init_ws);
+
+var ws_keepalive = null;
+
+function init_ws() {
 	var sock = new WebSocket("ws://localhost:57123/");
+	sock.onopen = function ()
+	{
+		$(".server-disconnected-container").fadeOut("fast", function() {
+			$(".container").fadeIn("fast");
+		});
+	};
 	sock.onmessage = function (evt) 
 	{ 
         var msg = $.parseJSON(evt.data);
-		if (game != null)
-			game.moveRemote(msg.issuer, msg.direction);
+		switch(msg.type)
+		{
+			case "input":
+				if (game != null)
+					game.moveRemote(msg.issuer, msg.direction);
+				break;
+			case "disconnected":
+				$(".container").fadeOut("fast", function() {
+					$(".chat-disconnected-container").fadeIn("fast");
+				});
+				break;
+			case "connected":
+				$(".chat-disconnected-container").fadeOut("fast", function() {
+					$(".container").fadeIn("fast");
+				});
+				break;
+		}
 	};
-	setInterval(function(){sock.send("{}")}, 5000);
-});
+	sock.onclose = function ()
+	{
+		if (ws_keepalive != null)
+		{
+			clearInterval(ws_keepalive);
+			ws_keepalive = null;
+		}
+		$(".container").fadeOut("fast", function() {
+			$(".server-disconnected-container").fadeIn("fast");
+		});
+		setTimeout(init_ws, 5000);
+	};
+	ws_keepalive = setInterval(function(){sock.send("{}")}, 5000);
+}
